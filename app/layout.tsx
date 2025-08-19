39ee7fdc38aa4ed88e03829831449104413c5a2f
ankan20 <adas34965@gmail.com>
2025-08-19 23:24:22 +0530

    Init:Added budget tracker with all inittial features

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "@/components/providers/RootProviders";
+import { Toaster } from "@/components/ui/sonner";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Budget Buddy",
+  description: "made by brkozkn999",
+};
+
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) 
+{
+  return (
+    <ClerkProvider>
+      <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+      <link rel="icon" href="/favicon.png" type="image/png"/>
+        <body className={inter.className}>
+          <Toaster richColors position='bottom-right' />
+          <RootProviders>{children}</RootProviders>
+          </body>
+      </html>
+    </ClerkProvider>
+  );
+}
